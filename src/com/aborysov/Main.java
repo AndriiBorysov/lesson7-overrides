97b8680f7265c4f3e0184a4d20e4f0a6dbebbe7c
andriy.borysov <andriy.borysov@ukr.net>
2021-04-09 20:34:11 +0300

    Function to power a number is implemented.

--- src/com/aborysov/Main.java
+++ src/com/aborysov/Main.java
@@ -5,4 +5,8 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
     }
+
+    public static double pow(double x, double y) {
+        return Math.pow(x, y);
+    }
 }
